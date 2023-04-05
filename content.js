b741cc86b760d5be533ee5d84071e750a0287e8f
arimura <kotaro_arimura@voyagegroup.com>
2023-04-05 17:31:31 +0900

    init

---
+++ content.js
@@ -0,0 +1,1 @@
+document.body.insertAdjacentHTML('afterbegin', '<div style="position: fixed; top: 10px; left: 10px; background-color: white; padding: 5px; border: 1px solid black; z-index: 9999;">hoge</div>');
