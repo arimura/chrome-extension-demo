8f82debd6f37f8c9661641ecc60185d275526e02
arimura <kotaro_arimura@voyagegroup.com>
2023-04-24 17:39:31 +0900

    Update chrome extension v3

--- v2/background.js
+++ v2/background.js
@@ -1,14 +1,27 @@
-chrome.browserAction.onClicked.addListener(tab => {
-    chrome.storage.sync.get("enabled", data => {
-      const enabled = !data.enabled;
-      chrome.storage.sync.set({ enabled });
-  
-      chrome.browserAction.setIcon({
-        path: enabled ? "icon-enabled.png" : "icon.png",
-        tabId: tab.id
-      });
-  
-      chrome.tabs.sendMessage(tab.id, { enabled });
+chrome.action.onClicked.addListener((tab) => {
+  chrome.storage.sync.get("enabled", (data) => {
+    const enabled = !data.enabled;
+    chrome.storage.sync.set({ enabled });
+
+    chrome.action.setIcon({
+      path: enabled ? "icon-enabled.png" : "icon.png",
+      tabId: tab.id
+    });
+
+    // Send a message to the content script using chrome.scripting.executeScript
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: sendMessageToContentScript,
+      args: [{ enabled }]
     });
   });
-  
+});
+
+// Function to send a message to the content script
+function sendMessageToContentScript({ enabled }) {
+  // Access the content script using the window object
+  const contentScript = window.contentScript;
+  if (contentScript) {
+    contentScript.postMessage({ enabled });
+  }
+}
