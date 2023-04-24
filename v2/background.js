231c6455196a13d125518a7d656eb16ae46a7218
arimura <kotaro_arimura@voyagegroup.com>
2023-04-24 20:31:05 +0900

    Add event on tab appear

--- v2/background.js
+++ v2/background.js
@@ -21,6 +21,32 @@ chrome.action.onClicked.addListener((tab) => {
   });
 });
 
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.onActivated.addListener((activeInfo) => {
+    const { tabId, windowId } = activeInfo;
+
+    chrome.tabs.get(tabId, (tab) => {
+      if (!tab.url.startsWith('http://') && !tab.url.startsWith('https://')) {
+        console.warn('Content script cannot be executed on this URL:', tab.url);
+        return;
+      }
+
+      chrome.storage.sync.get("enabled", (data) => {
+        const enabled = data.enabled;
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          function: sendMessageToContentScript,
+          args: [{ enabled }]
+        });
+      });
+
+      console.log('Active tab URL:', tab.url);
+    });
+  });
+});
+
+
+
 // Function to send a message to the content script
 function sendMessageToContentScript({ enabled }) {
   // Access the content script using the window object
